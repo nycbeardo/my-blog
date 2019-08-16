b6c7eadabb6b554330e41b3d00c9abaef615de5e
Montique Stevens <mstevens528@gmail.com>
2019-08-16 02:52:30 -0400

    Complete Overhaul

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,78 +1,28 @@
 module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
   siteMetadata: {
     title: `Objector`,
     author: `Montique Stevens`,
-    description: `My dev blog chronicling my journey.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `nycbeardo`,
-    },
+    description: `My dev blog chronicling my journey`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/nycbeardo`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/nycbeardo`,
+      },
+      {
+        name: `portfolio`,
+        url: `https://mstevens.xyz`,
+      },
+    ],
   },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Objector`,
-        short_name: `Objector`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#ffa7c4`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
 }
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+  Personal blog of <Styled.a href="https://mstevens.xyz/">Montique Stevens</Styled.a>,
+    {` `}
+    a Frontend Developer and design enthusiast based in New York City.
+    <br />
+   
+  </Fragment>
+)
