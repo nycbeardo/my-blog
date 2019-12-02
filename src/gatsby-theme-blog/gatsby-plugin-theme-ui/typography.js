535fe1eeaf255937390f05a4ca21a9a31a22258e
Montique Stevens <mstevens528@gmail.com>
2019-12-02 15:03:44 -0500

    Update typography.js

--- src/gatsby-theme-blog/gatsby-plugin-theme-ui/typography.js
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/typography.js
@@ -1,9 +1,15 @@
 import merge from "deepmerge"
 import defaultThemeTypography from "gatsby-theme-blog/src/gatsby-plugin-theme-ui/typography"
 
-export default merge({}, defaultThemeTypography, {
+export default merge(defaultThemeTypography, {
   fonts: {
-    body: "system-ui, serif",
-    heading: "system-ui, serif"
+    body: " 'Roboto Condensed', sans-serif",
+    heading: " 'Roboto', sans-serif",
+    monospace: "Menlo, monospace"
+  },
+  fontWeights: {
+    body: 400,
+    heading: 500,
+    bold: 700,
   }
 });
