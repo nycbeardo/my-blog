453be3660571dd8a844ef9e0e65d151b2c66abdd
Montique Stevens <mstevens528@gmail.com>
2019-12-02 15:15:28 -0500

    Update typography.js

--- src/gatsby-theme-blog/gatsby-plugin-theme-ui/typography.js
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/typography.js
@@ -9,7 +9,14 @@ export default merge(defaultThemeTypography, {
   },
   fontWeights: {
     body: 400,
-    heading: 500,
+    heading: 650,
     bold: 700,
-  }
+  },
+  lineHeights: {
+    body: 2.0,
+    heading: 1.275,
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+
 });
